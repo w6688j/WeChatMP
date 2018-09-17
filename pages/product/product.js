8a7f2b9aa12d64a1f488a9258a5d43afac163d8f
w6688j <15855463772w>
2018-09-17 21:01:22 +0800

    商品详情页

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,15 +1,65 @@
 // pages/product/product.js
+import {Product} from 'product-model.js';
+
+var product = new Product();  //实例化 商品详情 对象
 Page({
 
     /**
      * 页面的初始数据
      */
-    data: {},
+    data: {
+        id: null,
+        loadingHidden: false,
+        hiddenSmallImg: true,
+        countsArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        productCounts: 1,
+        currentTabsIndex: 0,
+        cartTotalCounts: 0,
+    },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
         let id = options.id;
-    }
+        this.data.id = id;
+        this._loadData();
+    },
+
+    /*加载所有数据*/
+    _loadData: function (callback) {
+        var that = this;
+        product.getDetailInfo(this.data.id, (data) => {
+            that.setData({
+                product: data,
+                loadingHidden: true
+            });
+            callback && callback();
+        });
+    },
+
+    //选择购买数目
+    bindPickerChange: function (e) {
+        this.setData({
+            productCounts: this.data.countsArray[e.detail.value],
+        })
+    },
+
+    //切换详情面板
+    onTabsItemTap:function(event){
+        var index=product.getDataSet(event,'index');
+        this.setData({
+            currentTabsIndex:index
+        });
+    },
+
+    /*添加到购物车*/
+    onAddingToCartTap: function (events) {
+        //防止快速点击
+        if (this.data.isFly) {
+            return;
+        }
+        this._flyToCartEffect(events);
+        this.addToCart();
+    },
 });
--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,66 +1,61 @@
-// pages/theme/theme.js
+import {Theme} from 'theme-model.js';
+
+var theme = new Theme(); //实例化  主题列表对象
 Page({
+    data: {
+        loadingHidden: false
+    },
+    onReady: function () {
+        wx.setNavigationBarTitle({
+            title: this.data.titleName
+        });
+    },
+    onLoad: function (option) {
+        this.data.titleName = option.name;
+        this.data.id = option.id;
+        wx.setNavigationBarTitle({
+            title: option.name
+        });
+        this._loadData();
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    },
 
-  },
+    /*加载所有数据*/
+    _loadData: function (callback) {
+        var that = this;
+        /*获取单品列表信息*/
+        theme.getProductorData(this.data.id, (data) => {
+            that.setData({
+                themeInfo: data,
+                loadingHidden: true
+            });
+            callback && callback();
+        });
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /*跳转到商品详情*/
+    onProductsItemTap: function (event) {
+        var id = theme.getDataSet(event, 'id');
+        wx.navigateTo({
+            url: '../product/product?id=' + id
+        })
+    },
 
-  },
+    /*下拉刷新页面*/
+    onPullDownRefresh: function () {
+        this._loadData(() => {
+            wx.stopPullDownRefresh()
+        });
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    //分享效果
+    onShareAppMessage: function () {
+        return {
+            title: '零食商贩 Pretty Vendor',
+            path: 'pages/theme/theme?id=' + this.data.id
+        }
+    }
 
-  },
+});
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
